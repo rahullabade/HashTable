90bebb0599c6d9bb4485f821d5797ae3e30440af
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-24 21:50:36 +0530

    Added: UC2_HashTable

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,24 +1,46 @@
 package hashTable;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class MyHashMap<K, V> {
-	LinkedList<K> myLinkList;
+	private final int numBuckets;
+	ArrayList<LinkedList<K>> myBucketArray;
 
 	MyHashMap() {
-		this.myLinkList = new LinkedList<>();
+		this.numBuckets = 10;
+		this.myBucketArray = new ArrayList<>(numBuckets);
+		// Create Empty Buckets LinkedList
+		for (int i = 0; i < numBuckets; i++)
+			this.myBucketArray.add(null);
+	}
+
+	private int getBucketIndex(K key) {
+		int indexOfHasCode = Math.abs(key.hashCode());
+		int index = indexOfHasCode % numBuckets;
+		return index;
 	}
 
 	public V get(K key) {
-		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkList.search(key);
+		int index = this.getBucketIndex(key);
+		LinkedList<K> myList = this.myBucketArray.get(index);
+		if (myList == null)
+			return null;
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myList.search(key);
 		return (myMapNode == null) ? null : myMapNode.getValue();
 	}
 
 	public void add(K key, V value) {
-		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkList.search(key);
+		int index = this.getBucketIndex(key);
+		LinkedList<K> LinkedList = this.myBucketArray.get(index);
+		if (LinkedList == null) {
+			LinkedList = new LinkedList<>();
+			this.myBucketArray.set(index, LinkedList);
+		}
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) LinkedList.search(key);
 		if (myMapNode == null) {
 			myMapNode = new MyMapNode<>(key, value);
-			this.myLinkList.append(myMapNode);
+			LinkedList.append(myMapNode);
 		} else {
 			myMapNode.setValue(value);
 		}
@@ -26,6 +48,6 @@ public class MyHashMap<K, V> {
 
 	@Override
 	public String toString() {
-		return "MyHashMapNodes{" + myLinkList + "}";
+		return "MyHashMapNodes{" + myBucketArray + '}';
 	}
 }
